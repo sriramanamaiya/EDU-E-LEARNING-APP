5436a409ddb0c8088f451ed494c5a5b25d79b952
sriramana <sriramanamaiya99@gmail.com>
2021-11-30 17:47:01 +0530

    added releasedate picker in create course comp

---
+++ src/components/Common-Module/Course-Module/DatePicker.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import TextField from '@mui/material/TextField'
+import AdapterDateFns from '@mui/lab/AdapterDateFns'
+import LocalizationProvider from '@mui/lab/LocalizationProvider'
+import DatePicker from '@mui/lab/DatePicker'
+import { Box } from '@mui/system'
+
+const DatePickerComp = (props) => {
+    const { value, handleDatePicker } = props
+
+    return (
+        <LocalizationProvider dateAdapter={AdapterDateFns}>
+            <DatePicker
+                label="Pick Release Date"
+                value={value}
+                minDate={new Date()}
+                onChange={(date) => {
+                    handleDatePicker(date)
+                }}
+                renderInput={(params) => <TextField  {...params} error={false} margin="normal" />}
+                />
+        </LocalizationProvider>
+    )
+}
+
+export default DatePickerComp
