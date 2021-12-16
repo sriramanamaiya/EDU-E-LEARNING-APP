a0a0fa64dca88d94a5e68f1d3c5bdc4864adc178
sriramana <sriramanamaiya99@gmail.com>
2021-12-16 16:12:43 +0530

    updated the navBar

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Container } from '@mui/material'
 
 import './App.css'
 
-import RouteComp from './components/NavBar/RouteComp'
+import RouteComp from './components/navBar/RouteComp'
 
 const App = (props) => {
 
