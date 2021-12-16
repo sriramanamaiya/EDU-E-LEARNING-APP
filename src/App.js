941c0640937426a8268db58cd02640474b852e04
sriramana <sriramanamaiya99@gmail.com>
2021-12-16 19:05:41 +0530

    Versioning untracked files

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Container } from '@mui/material'
 
 import './App.css'
 
-import RouteComp from './components/navBar/RouteComp'
+import RouteComp from './components/NavBar/RouteComp'
 
 const App = (props) => {
 
